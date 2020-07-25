384d713b3b277ed8acb7bcb897b5a5a4c6b9c2fd
aymanMahgoub <ayman.mahgoub1@gmail.com>
2020-07-25 09:51:16 +0200

    [create] EventConstant file hold all event constants keys

---
+++ src/Constant/EventConstant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Constant;
+
+/**
+ * Class EventConstant
+ *
+ * @package App\Constant
+ */
+final class EventConstant
+{
+    const EMPLOYEE_NAME = 'employee_name';
+    const EMPLOYEE_MAIL = 'employee_mail';
+    const EVENT_ID      = 'event_id';
+    const EVENT_NAME    = 'event_name';
+    const FEE           = 'participation_fee';
+    const EVENT_DATE    = 'event_date';
+
+}
